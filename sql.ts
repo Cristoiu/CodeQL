ea688a778888edc75e14aaaf065faf74a3195f66
Rafael Estefan <rafaelestefan@MacBook-Air-de-Rafael.local>
2024-12-11 10:29:52 -0300

    create dependency-review

--- sql.ts
+++
@@ -1,8 +0,0 @@
-const query = 'SELECT * FROM users WHERE username = $1';
-client.query(query, [username], (err, res) => {
-  if (err) {
-    console.error('Erro ao buscar o usuário:', err);
-  } else {
-    console.log('Dados do usuário:', res.rows);
-  }
-});
